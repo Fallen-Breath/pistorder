56d253aa8622d08e84187f6296eabe6ad065be8e
Fallen_Breath <yqx791125833@gmail.com>
2021-02-24 19:37:11 +0800

    modify the hotkey from o to p and update readme

--- src/main/java/me/fallenbreath/pistorder/utils/PistorderKeyBinding.java
+++ src/main/java/me/fallenbreath/pistorder/utils/PistorderKeyBinding.java
@@ -8,11 +8,12 @@ import java.util.List;
 
 public class PistorderKeyBinding
 {
-	public static final KeyBinding CLEAR_DISPLAY_KEY = new KeyBinding("pistorder.clear_display", InputUtil.fromName("key.keyboard.o").getKeyCode(), "key.categories.misc");
+	public static final KeyBinding CLEAR_DISPLAY_KEY = new KeyBinding("pistorder.clear_display", InputUtil.fromName("key.keyboard.p").getKeyCode(), "key.categories.misc");
 
 	public static KeyBinding[] updateVanillaKeyBinding(KeyBinding[] keysAll)
 	{
 		List<KeyBinding> list = Lists.newArrayList(keysAll);
+		list.remove(CLEAR_DISPLAY_KEY);
 		list.add(CLEAR_DISPLAY_KEY);
 		return list.toArray(new KeyBinding[0]);
 	}
